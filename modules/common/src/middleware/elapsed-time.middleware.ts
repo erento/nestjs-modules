33a1843859c5218fe24b3dbce760e62dc62fff71
Matic Vrenko <matic.vrenko@erento.com>
2021-09-07 16:53:23 +0200

    Only string on Route started

--- modules/common/src/middleware/elapsed-time.middleware.ts
+++ modules/common/src/middleware/elapsed-time.middleware.ts
@@ -33,9 +33,7 @@ export class ElapsedTimeMiddleware implements NestMiddleware {
             });
         });
 
-        this.logger.log({
-            message: 'Route started',
-        });
+        this.logger.log('Route started');
         next();
     }
 }
