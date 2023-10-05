f64ce14375aa46fda1298af57d3c6dbaea06175c
Aljaz Babic <aljaz.babic@erento.com>
2023-10-05 08:19:34 +0200

    feature/DEV-7218 add additional erento enums

--- modules/common/src/pipes/constants/index.ts
+++ modules/common/src/pipes/constants/index.ts
@@ -6,6 +6,20 @@ export enum ErentoLocale {
     EnUs = 'en-US',
 }
 
+export const ErentoGermanLocales = [
+    ErentoLocale.DeDe,
+    ErentoLocale.DeCh,
+    ErentoLocale.DeAt,
+];
+
+export const languageHosts: Record<ErentoLocale, string> = {
+    [ErentoLocale.DeAt]: 'https://www.erento.at',
+    [ErentoLocale.DeCh]: 'https://www.erento.ch',
+    [ErentoLocale.DeDe]: 'https://www.erento.com',
+    [ErentoLocale.EnGb]: 'https://www.erento.co.uk',
+    [ErentoLocale.EnUs]: 'https://www.erento.net',
+};
+
 export enum CampandaLocale {
     DeDe = 'de_DE',
     DeCh = 'de_CH',
