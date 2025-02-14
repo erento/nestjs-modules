5336697f33d986e367f2da82d1bea4b870c46a4e
Blaž Novak <blaz.novak@erento.com>
2025-02-14 07:51:39 +0100

    fix response structure

--- modules/salesforce/src/user-salesforce-response.ts
+++ modules/salesforce/src/user-salesforce-response.ts
@@ -1,21 +1,9 @@
-export const userSalesforceResponse: (
-  acknowledged: boolean,
-  crmSfAccountId: string,
-  e1Id: string,
-  e2UserId: string,
-  userKey: string,
-) => string = (acknowledged: boolean, crmSfAccountId: string, e1Id: string, e2UserId: string, userKey: string): string =>
+export const userSalesforceResponse: (successfulUpsert: boolean) => string = (successfulUpsert: boolean): string =>
     `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
   <soapenv:Body>
-    <update xmlns="urn:partner.soap.sforce.com">
-      <sObjects xsi:type="Account">
-        <Ack>${acknowledged}</Ack>
-        <Id>${crmSfAccountId}</Id>
-        <Uid__c>${e1Id}</Uid__c>
-        <E2UserId__c>${e2UserId}</E2UserId__c>
-        <MyErentoLink__c>https://intranet.erento.com/?adminmenu=626&process=658&userKey=${userKey}</MyErentoLink__c>
-        <SdpUrl__c>https://www.erento.com/anbieter/${e2UserId}</SdpUrl__c>
-      </sObjects>
-    </update>
+    <notificationsResponse xmlns="http://soap.sforce.com/2005/09/outbound">
+      <Ack>true</Ack>
+      <Success>${successfulUpsert}</Success>
+    </notificationsResponse>
   </soapenv:Body>
 </soapenv:Envelope>`;
