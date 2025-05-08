d16dce3b0cd89a2a26ed68e906eea557e490b07d
blaznovak99 <blaz.novak@erento.com>
2025-05-08 09:11:16 +0200

    add error response to sf

--- modules/salesforce/src/user-salesforce-response.ts
+++ modules/salesforce/src/user-salesforce-response.ts
@@ -1,9 +1,13 @@
-export const userSalesforceResponse: (successfulUpsert: boolean) => string = (successfulUpsert: boolean): string =>
+export const userSalesforceResponse: (
+  successfulUpsert: boolean,
+  errorMessage: string,
+) => string = (successfulUpsert: boolean, errorMessage: string): string =>
     `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
   <soapenv:Body>
     <notificationsResponse xmlns="http://soap.sforce.com/2005/09/outbound">
       <Ack>true</Ack>
       <Success>${successfulUpsert}</Success>
+      <ErrorMessage>${errorMessage}</ErrorMessage>
     </notificationsResponse>
   </soapenv:Body>
 </soapenv:Envelope>`;
